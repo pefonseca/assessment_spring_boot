d3c6e54c9a34bccfbe54acc22e7d2a701422dc0f
Pedro Fonseca Nunes <pedrofonunes@gmail.com>
2024-06-18 00:38:00 -0300

    feat: ajustes finais.

--- src/main/java/infnet/spring/boot/assessment/api/rest/controller/NotaController.java
+++ src/main/java/infnet/spring/boot/assessment/api/rest/controller/NotaController.java
@@ -5,6 +5,7 @@ import infnet.spring.boot.assessment.api.rest.dto.response.NotaResponseDTO;
 import infnet.spring.boot.assessment.domain.service.NotaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class NotaController {
     private NotaService notaService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('PROFESSOR')")
     private ResponseEntity<NotaResponseDTO> adicionarNotaParaAluno(@RequestBody NotaRequestDTO notaRequestDTO) {
         return ResponseEntity.ok(notaService.adicionarNotaParaAluno(notaRequestDTO));
     }
